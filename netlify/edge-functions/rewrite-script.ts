8bdb9fdab84bc71fcaf5ae985187be0485bfccd1
Sunny Zanchi <sunnyz@hey.com>
2024-03-07 12:27:57 -0500

    update function

--- netlify/edge-functions/rewrite-script.ts
+++ netlify/edge-functions/rewrite-script.ts
@@ -1,14 +1,11 @@
 import type { Config, Context } from "@netlify/edge-functions";
-import { Readable } from 'stream'
 
 export default async function handler(req: Request, context: Context) {
-  const body = await req.text()
+  const response = context.next();
+  const body = await response.text()
   const reply = body.replace('REPLACE_ME_BLEASE', 'https://unpkg.com/jquery@3.3.1/dist/jquery.min.js')
-  const stream = new Readable();
-  stream.push(reply)
-  stream.push(null)
 
-  return new Response(stream, {
+  return new Response(reply, {
     status: 200,
     headers: { "Content-Type": "text/html" },
   });
